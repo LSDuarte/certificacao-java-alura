b9ef7bc18fbef6abceadd3340de85c7857729833
Larissa Duarte <42499@pjmt.local>
2020-08-07 17:49:35 -0400

    07/08/2020 - Iniciando Java Básico
    (Estrutura basica, Pacotes e Imports)

---
+++ src/alura/basico/ClassePessoa.java
@@ -0,0 +1,17 @@
+package alura.basico;
+
+public class ClassePessoa {
+
+	String nome;
+
+	public ClassePessoa(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		String cursoNome = "Certificação Java";
+		return cursoNome;
+	}
+	
+	// 7:09 min do video DEFINA A ESTRUTURA DE UMA CLASSE JAVA
+}
---
+++ src/alura/basico/EstruturaDaClasse.java
@@ -0,0 +1,52 @@
+package alura.basico; // pacote, primeira coisa que vem no arquivo java - é opcional
+//1º coisa --- PACKAGE
+
+// package br.com.caelum.certificacao
+// estrutura da classe java 
+
+/**
+ * @author LSDuarte
+ *
+ */
+
+// 2º coisa --- IMPORTS // sempre depois do package
+import java.util.Date;
+import java.io.*;
+
+
+// 3º coisa ---- CLASSES
+public class EstruturaDaClasse {
+
+}
+
+// 4º coisa -- INTERFACES
+interface Figura{}
+
+// cometário vem em qualquer lugar.
+
+// uma linha
+
+/*
+ * Intervalo de comentário (Tudo que estiver aqui)
+ * 
+ */
+
+/**
+ * 
+ * COMENTÁRIO JAVADOC 
+ * SERVE PARA DOCUMENTAR O CÓDIGO
+ * 
+ */
+
+
+// TODAS OPCIONAIS
+// ARQUIVO VAZIO COMPILA
+
+
+
+
+// QUANDO ARQUIVO NÃO POSSUI NOME DE PACOTE, SIGNIFICA QUE ESTÁ NO PACOTE PADRÃO "RAIZ" - PACOTE DEFAULT
+
+
+
+
